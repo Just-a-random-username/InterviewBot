4bfe5c77fd805f895bcd1df6c8c8c7b976bb9408
Just-a-random-username <arjunagnihotri0211@gmail.com>
2025-04-07 02:58:07 +0530

    Fixing middleware

--- backend/middleware/auth.py
+++ backend/middleware/auth.py
@@ -1,30 +1,33 @@
 from flask import Blueprint, request, jsonify
 import jwt
 from functools import wraps
-from models.user import User  # Assuming your User model is defined with SQLAlchemy
+import app
+from utils.connectdb_utils import connectDb
+from bson import ObjectId
 
 auth_bp = Blueprint('auth_bp', __name__)
-secret_key = 'lodekasignature'
+
+database = connectDb()
+user_collection = database['users']
 
 
 def middleware(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         try:
-            token = request.cookies.get('token')
+            token = request.headers.get('token')
 
             if not token:
                 return jsonify({"message": "Token is missing"}), 401
 
             try:
-                decoded_data = jwt.decode(token, secret_key, algorithms=["HS256"])
+                decoded_data = jwt.decode(token, app.app.config['SECRET_KEY'], algorithms=["HS256"])
             except jwt.ExpiredSignatureError:
                 return jsonify({"message": "Token has expired"}), 401
             except jwt.InvalidTokenError:
                 return jsonify({"message": "Invalid token"}), 401
 
-            user = User.query.filter_by(email=decoded_data.get('email')).first()
-
+            user = user_collection.find_one({'_id':ObjectId(decoded_data)})
             if not user:
                 return jsonify({"message": "User not found"}), 401
 
